bb6a1cab2008b61ee9cfb969f4ae31950aecd3b4
MinexAutomation <minex.automation@gmail.com>
2019-10-23 21:28:34 -0700

    Added executable file path provider extension method that uses a Lombardy stringly-typed path operator instance to get the parent directory of the executable file as the executable file's directory path.

--- source/R5T.Macommania.Lombardy/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Macommania.Lombardy
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Macommania.Lombardy/Code/Services/Extensions/IExecutableFilePathProviderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using R5T.Lombardy;
+
+
+namespace R5T.Macommania.Lombardy.Extensions
+{
+    public static class IExecutableFilePathProviderExtensions
+    {
+        /// <summary>
+        /// Get the path of the directory containing the executable file.
+        /// </summary>
+        public static string GetExecutableFileDirectoryPath(this IExecutableFilePathProvider executableFilePathProvider, IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            var executableFilePath = executableFilePathProvider.GetExecutableFileDirectoryPath();
+
+            var executableFileDirectoryPath = stringlyTypedPathOperator.GetDirectoryPathForFilePath(executableFilePath);
+            return executableFileDirectoryPath;
+        }
+    }
+}
